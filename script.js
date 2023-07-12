6d7683c8b2e673c6da1b4f42314eeabcb7a460ff
JHANSIRANI <jhansiraniakunoori@gmail.com>
2023-07-12 13:27:21 +0530

    first commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Science", "Cloud EnginEer"],
+        strings: ["Data Science", "Cloud Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
